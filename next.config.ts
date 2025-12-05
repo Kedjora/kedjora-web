76b0e3e4c4d0b6a06e6e19d7bd0c39ef5449f7db
Prass <pras.ari69@gmail.com>
2025-12-06 05:54:09 +0700

    style: standardize quotes and formatting in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,34 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin('./app/i18n/request.ts');
+const withNextIntl = createNextIntlPlugin("./app/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  compress: true,
+  reactStrictMode: true,
+  poweredByHeader: false,
   images: {
+    formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        protocol: "https",
+        hostname: "picsum.photos",
       },
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'grainy-gradients.vercel.app',
+        protocol: "https",
+        hostname: "grainy-gradients.vercel.app",
       },
       {
-        protocol: 'https',
-        hostname: 'ui-avatars.com',
+        protocol: "https",
+        hostname: "ui-avatars.com",
       },
       {
-        protocol: 'https',
-        hostname: 'kedjora.vercel.app',
+        protocol: "https",
+        hostname: "kedjora.vercel.app",
       },
     ],
   },
